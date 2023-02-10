fe2c85553917ce975b7b564f517f09ab5f8e22b2
jhon11335 <125082754+jhon11335@users.noreply.github.com>
2023-02-10 20:04:10 +0000

    Add files via upload

---
+++ whitelist.bat
@@ -0,0 +1,22 @@
+
+@echo off
+
+cd "%~dp0"
+start SynapseX.exe
+
+@echo off
+
+start "%~dp0SynapseX.exe"
+
+
+@echo off
+set errorCode=%1
+set errorMessage=
+
+if %errorCode% == 1 set errorMessage=Error: Invalid input
+if %errorCode% == 2 set errorMessage=Error: File not found
+if %errorCode% == 3 set errorMessage=Error: Insufficient permissions
+
+echo %errorMessage%
+echo The program will now exit.
+pause
